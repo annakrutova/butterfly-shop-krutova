2924cdd2741bc9ca5b03fbb4037eb3f3f8a70c33
Anna Krutova <knop_k_a@mail.ru>
2021-11-23 23:05:16 +0300

    сделала корректное позиционирование

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,13 @@
 console.log('script work!');
+
+$('.contacts-tab-link').on('click', function (e) {
+  e.preventDefault();
+
+  let index = $(this).index('.contacts-tab-link');
+
+  $('.contacts-tab-link').removeClass('active');
+  $(this).addClass('active');
+
+  $('.contacts-content').removeClass('active');
+  $('.contacts-content').eq(index).addClass('active');
+});
